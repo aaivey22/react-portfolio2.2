766acdb308bd68a1151d789707ecd2b0f3937fd7
aaivey22 <angelawings22@gmail.com>
2020-07-25 23:09:43 -0400

    Updates

---
+++ precache-manifest.9293eab0f9f39d2004a21f3332afd7e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef960cd4e21e2c2e63a4ffd46f63d97e",
+    "url": "/react-portfolio2.2/index.html"
+  },
+  {
+    "revision": "a986ae3869a6030bc76c",
+    "url": "/react-portfolio2.2/static/css/main.21d3987f.chunk.css"
+  },
+  {
+    "revision": "dfcae7cd3b96575b3d00",
+    "url": "/react-portfolio2.2/static/js/2.d7c33d20.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/react-portfolio2.2/static/js/2.d7c33d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a986ae3869a6030bc76c",
+    "url": "/react-portfolio2.2/static/js/main.fc030d0c.chunk.js"
+  },
+  {
+    "revision": "40dcbd1c390bf46009fb",
+    "url": "/react-portfolio2.2/static/js/runtime-main.982a7269.js"
+  },
+  {
+    "revision": "116c70b46eb1bf16a13e1e6b3261998e",
+    "url": "/react-portfolio2.2/static/media/avatar.116c70b4.png"
+  },
+  {
+    "revision": "ac172f94d6d1d166f3c72250e68bdcac",
+    "url": "/react-portfolio2.2/static/media/rockymtn.ac172f94.jpg"
+  }
+]);
