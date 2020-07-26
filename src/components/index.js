41676c946ffea9620ea08e4f561ff87bb924cc34
aaivey22 <angelawings22@gmail.com>
2020-07-25 21:20:38 -0400

    added particles to indexJS for cool effect

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,64 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Header from './Header';
+import Particles from 'react-particles-js';
+import {makeStyles} from "@material-ui/styles";
+
+const useStyles = makeStyles({
+    partCanvas: {
+        position: "absolute"
+    }
+})
 
 const About = () => {
+    const classes = useStyles()
     return (
-        <div>
+        <>
           <Navbar />
           <Header />
-        </div>
-    )
+        <Particles
+        canvasClassName={classes.partCanvas}
+            params={{
+                particles: {
+                    number: {
+                        value: 45,
+                        density: {
+                            enable: true,
+                            value_area: 900
+                        }
+                    },
+                    shape: {
+                        type: "star",
+                        stroke: {
+                            width: 1,
+                            color: "#615ebd"
+                        }
+                    },
+                    size: {
+                        value: 8,
+                        random: true,
+                        animation: {
+                            enable: true,
+                            speed: 6,
+                            size_min: 0.1,
+                            sync: true
+                        }
+                    },
+                    opacity: {
+                        value: 1,
+                        random: true,
+                        anim: {
+                            enable: true,
+                            speed: 1,
+                            opacity_min: 0.1,
+                            sync: false
+                        }
+                    }
+                }
+            }}
+        />
+        </>
+    );
 };
 
 export default About;
